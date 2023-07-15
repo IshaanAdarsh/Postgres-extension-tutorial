c55814611ca4a6ec9dab964d29dd8ae7e110e48b
Ishaan Adarsh <100434702+IshaanAdarsh@users.noreply.github.com>
2023-07-15 12:30:53 +0530

    Code for the C-based Extension

---
+++ Code/pg_cext/Makefile
@@ -0,0 +1,7 @@
+MODULES = pg_cext
+EXTENSION = pg_cext
+DATA = pg_cext--1.0.0.sql
+
+PG_CONFIG = pg_config
+PGXS := $(shell $(PG_CONFIG) --pgxs)
+include $(PGXS)
---
+++ Code/pg_cext/pg_cext.c
@@ -0,0 +1,15 @@
+#include "postgres.h"
+#include "fmgr.h"
+
+PG_MODULE_MAGIC;
+
+PG_FUNCTION_INFO_V1(add);
+
+Datum
+add(PG_FUNCTION_ARGS)
+{
+    int32 arg1 = PG_GETARG_INT32(0);
+    int32 arg2 = PG_GETARG_INT32(1);
+
+    PG_RETURN_INT32(arg1 + arg2);
+}

e46fc9142e3fc2566525f5dd0ba3338032366e94
Ishaan Adarsh <100434702+IshaanAdarsh@users.noreply.github.com>
2023-07-26 01:01:47 +0530

    Add files via upload

---
+++ Code/pg_py_ext/pg_py_ext--1.0.sql
@@ -0,0 +1,10 @@
+-- pg_py_ext--1.0.sql
+-- Create the function that uses the Python script
+CREATE OR REPLACE FUNCTION add_numbers(a integer, b integer)
+RETURNS integer
+LANGUAGE plpython3u
+AS $$
+def add_numbers(a, b):
+    return a + b
+return add_numbers(int(a), int(b))
+$$;

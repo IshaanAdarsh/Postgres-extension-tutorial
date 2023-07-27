7401dff57de521fee37386f3468899dfff4ce6db
Ishaan Adarsh <100434702+IshaanAdarsh@users.noreply.github.com>
2023-07-27 22:46:08 +0530

    Add files via upload

---
+++ Code/pg_plpgsql_ext/pg_plpgsql_ext--1.0.sql
@@ -0,0 +1,10 @@
+-- pg_plpgsql_ext--1.0.sql
+-- Create the function that uses PL/pgSQL
+CREATE OR REPLACE FUNCTION subtract_numbers(a integer, b integer)
+RETURNS integer
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    RETURN a - b;
+END;
+$$;
